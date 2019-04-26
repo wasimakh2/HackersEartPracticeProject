0948f76c0113e845b96d1a317d6f1de1b3e5cc5c
Wasim Akhtar <wasim.akh2@gmail.com>
2019-04-27 03:17:07 +0530

    Updated

--- HackersEartPracticeProject/Program.cs
+++ HackersEartPracticeProject/Program.cs
@@ -6,7 +6,39 @@ namespace HackersEartPracticeProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a = Console.ReadLine();
+            int b;
+           int result= int.TryParse(a, out b)?int.Parse(a):0;
+
+
+           for (int k = 1; k <=result; k++)
+           {
+               if (Check_Prime(k) == 1)
+               {
+                   Console.Write(k+" ");
+               }
+           }
+
+           Console.ReadLine();
+        }
+
+
+        private static int Check_Prime(int number)
+        {
+            int i;
+            for (i = 2; i <= number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    return 0;
+                }
+            }
+            if (i == number)
+            {
+                return 1;
+            }
+            return 0;
         }
     }
 }
+
---
+++ SeatingArrangment/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace SeatingArrangment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Seat
+    {
+        public int SeatNumber { get; set; }
+        public string SeatType { get; set; }
+        public int FaceSeatNumber { get; set; }
+    }
+    class SeatingArrangement
+    {
+
+        public void GetSeatList()
+        {
+            bool SeatFlag = false;
+            List<Seat> mainList = new List<Seat>();
+            for (int i = 1; i <= 108; i++)
+            {
+                Seat seat = new Seat();
+                seat.SeatNumber = i;
+
+                mainList.Add(seat);
+            }
+
+
+        }
+    }
+}
